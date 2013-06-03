206c9e5e00823dd7a6676f9f4115d3fa28b938fe
Paul <paul.varnish@altoenergy.com>
2013-06-03 17:44:32 +0200

    better debug labelling

--- objective.py
+++ objective.py
@@ -28,13 +28,13 @@ def returns(portfolio, F__):
 def sharp(portfolio, F__):
     sumR = sumR2 = 0
     logging.debug("c_ %s" % portfolio.c_)
-    logging.debug("F_, r_, R")
-    logging.debug("%s, ," % F__[0])
+    logging.debug("F__[t-1], r__[t], R[t]")
     for t in range(1, portfolio.tMax):
         R = np.dot(F__[t - 1], portfolio.r__[t]) - np.dot(portfolio.c_, np.absolute(F__[t] - F__[t - 1]))
-        logging.debug("%s, %s, %s" % (F__[t], portfolio.r__[t], R))
+        logging.debug("%s, %s, %s" % (F__[t - 1], portfolio.r__[t], R))
         sumR += R
         sumR2 += R * R
+    logging.debug("%s, ," % F__[-1])
     A = sumR / portfolio.tMax
     B = sumR2 / portfolio.tMax
     logging.debug("A %s, B %s" % (A, B))
